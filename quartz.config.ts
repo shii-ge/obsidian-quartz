c896586924ca18f1ed42091695ed543e34cdd314
Gabriel S <gabrielfelipeshigio@gmail.com>
2025-04-16 10:17:00 -0300

    Quartz sync: Apr 16, 2025, 10:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "!(res)**/!(*.md)", "!(*.md)"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
